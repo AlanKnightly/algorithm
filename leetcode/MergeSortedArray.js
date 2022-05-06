86b7aa9fc292673001428642ed5deff354205f4c
shenjiangping <shenjiangping@sdbattery.com>
2022-05-06 13:38:13 +0800

    feat: 排序

--- leetcode/MergeSortedArray.js
+++ leetcode/MergeSortedArray.js
@@ -3,10 +3,10 @@ var merge = function(nums1, m, nums2, n) {
     while(p){
       console.log('p1',p1,'p2',p2)
       if(nums1[p1]>=nums2[p2]){
-        nums1[m - counter] = nums1[p1]
+        nums1[p] = nums1[p1]
         p1--
       }else{
-        nums1[m - counter] = nums2[p2]
+        nums1[p] = nums2[p2]
         p2--
       }
       p--
@@ -16,3 +16,4 @@ var merge = function(nums1, m, nums2, n) {
 
 const nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
 console.log(merge(nums1, m, nums2,n))
+
