1da64243a53a34191429a43e9651ab58a9f55fba
shenjiangping <shenjiangping@sdbattery.com>
2022-06-03 12:18:57 +0800

    feat: update

---
+++ demo/hello-world/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app" @click="click">{{ num }}</div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      num: 0,
+    };
+  },
+  onMounted() {
+    this.$nextTick().then(() => console.log("set at mounted"));
+  },
+  methods: {
+    click() {
+      const trackNum = this.num++;
+      this.$nextTick().then((r) => {
+        console.log("r:", r);
+        console.log(`set at num:${trackNum}`);
+      });
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ design/chain.js
@@ -0,0 +1,36 @@
+
+
+class App {
+    constructor() {
+        this.middleware = []
+    }
+    use(fn) {
+        this.middleware.push(fn)
+    }
+    handle(req) {
+        let nextIndex = -1
+        const next = () => {
+            nextIndex++
+            if (this.middleware[nextIndex]) {
+                this.middleware[nextIndex].call(null, req, next)
+            }
+        }
+        next()
+    }
+}
+
+const app = new App()
+app.use(function (req, next) {
+    console.log('1', req)
+    next()
+})
+app.use(function (req, next) {
+    console.log('2', req)
+    next()
+})
+app.use(function (req, next) {
+    console.log('3', req)
+    next()
+})
+
+app.handle({ params: 123 })
